b14e1e603b847add2a8d553889b3284012081ed4
Christopher Cruz Collazo <37718616+ChristopherCruz21@users.noreply.github.com>
2022-11-02 16:42:42 -0400

    Data elements added to InfoElements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,31 @@
-import React, {useState} from 'react'
-import HeroSection from '../components/HeroSection';
-import InfoSection from '../components/InfoSection';
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import React, { useState } from "react";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import {
+  homeObjOne,
+  homeObjThree,
+  homeObjTwo,
+} from "../components/InfoSection/Data";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    }
-
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-        <>
-            <Sidebar isOpen={isOpen} toggle={toggle} />
-            <Navbar toggle={toggle} />
-            <HeroSection />
-            <InfoSection />
-        </>
-    )
-}
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+};
 
-export default Home
+export default Home;
